62b0f94b782f4844a780350d707bbf030417c4b1
Danielsong007 <Danielsong007@gmail.com>
2025-04-02 13:56:51 +0800

    1

--- sensors/dayang.py
+++ sensors/dayang.py
@@ -4,6 +4,9 @@ import time
 class DaYangSensor:
     def __init__(self):
         self.ser = serial.Serial(port="/dev/ttyUSB1", baudrate=19200, bytesize=8, parity=serial.PARITY_NONE, stopbits=1, timeout=1)
+        self.ser.write(bytes([0x01, 0x10, 0x06, 0x2A, 0x00, 0x02, 0x04, 0x00, 0x00, 0x00, 0x01, 0x9B, 0xA8])) # Zero
+        # response = self.ser.read(8)  # 读取传感器应答命令
+        # print(f"Response in HEX: {response.hex().upper()}")
 
     def read_angles(self): # 读取传感器角度数据
         self.ser.flushInput()  # 清空串口接收缓冲区
@@ -23,7 +26,7 @@ if __name__ == "__main__":
     try:
         while True:
             force=sensor.read_angles()-force_init
-            print(force)
+            print(force,force_init)
             time.sleep(0.01)  # 等待 100ms，确保命令已发送
     except KeyboardInterrupt:
         print("KeyboardExit")
