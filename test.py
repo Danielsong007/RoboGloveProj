d6983a4f0b47d1abe4b922f69e3c3ee9cd74da0c
Danielsong007 <Danielsong007@gmail.com>
2025-10-31 14:01:56 +0800

    1

--- test.py
+++ test.py
@@ -1,8 +1,4 @@
 import numpy as np
 
-arr = np.array([0.80872643], dtype=np.float32)
-
-# 提取第一个元素
-value = arr[0]
-print(value)  # 0.80872643
-print(type(value))  # <class 'numpy.float32'>
+a=6
+if a>1: print(a)
--- z_rope_jog_Measure_ligan_dynamic.py
+++ z_rope_jog_Measure_ligan_dynamic.py
@@ -8,17 +8,38 @@ from collections import deque
 import threading
 import socket
 
+Rope_S = 0
+Touch_S = 0
+cur_pos_abs = 0
+
+buffer_dyn_Srope = deque([0.0]*3, maxlen=3)
+buffer_weight_Srope = deque([0.0]*50, maxlen=50)
+buffer_dyn_Stouch = deque([0.0]*3, maxlen=3)
+buffer_weight_Stouch = deque([0.0]*50, maxlen=50)
+buffer_rising_CurPos = deque([0]*5, maxlen=5)
+rising_slope = 0
+
+def read_cur_pos(myXYZ, InitPos):
+    global cur_pos_abs
+    global buffer_rising_CurPos
+    global rising_slope
+    while True:
+        cur_pos_abs = myXYZ.Get_Pos(3)+InitPos
+        buffer_rising_CurPos.append(cur_pos_abs)
+        x = np.arange(len(buffer_rising_CurPos))*1000
+        rising_slope, _ = np.polyfit(x, -np.array(buffer_rising_CurPos), 1)
+        time.sleep(0.001)
 
 def read_rope_sensor():
     global Rope_S
-    global Ave_Rope_S
-    Srope_buffer = deque(maxlen=3)  # 自动丢弃旧数据
+    global buffer_dyn_Srope
+    global buffer_weight_Srope
     Srope = DaYangSensor('/dev/ttyUSB0',0)
     try:
         while True:
             Rope_S = Srope.read_angles()
-            Srope_buffer.append(Rope_S)
-            Ave_Rope_S=np.mean(Srope_buffer)
+            buffer_dyn_Srope.append(Rope_S)
+            buffer_weight_Srope.append(Rope_S)
             time.sleep(0.001)
     except KeyboardInterrupt:
         Srope.close()
@@ -26,8 +47,8 @@ def read_rope_sensor():
 
 def touch_sensor_server(host='0.0.0.0', port=65432):
     global Touch_S
-    global Ave_Touch_S
-    Stouch_buffer = deque(maxlen=3)  # 自动丢弃旧数据
+    global buffer_dyn_Stouch
+    global buffer_weight_Stouch
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((host, port))
         s.listen()
@@ -41,28 +62,8 @@ def touch_sensor_server(host='0.0.0.0', port=65432):
                     break
                 msg = data.decode()
                 Touch_S = int(msg.split()[-1])
-                Stouch_buffer.append(Touch_S)
-                Ave_Touch_S=np.mean(Stouch_buffer)
-
-Rope_S = 0
-Touch_S = 0
-Ave_Rope_S = 0
-Ave_Touch_S = 0
-threading.Thread(target=read_rope_sensor, args=(), daemon=True).start()
-threading.Thread(target=touch_sensor_server, args=(), daemon=True).start()
-
-# def is_motor_rising(positions):
-#     if len(positions) < 2:
-#         return False
-#     # 计算位置变化趋势（使用线性回归斜率）
-#     x = np.arange(len(positions))
-#     slope, _ = np.polyfit(x, positions, 1)
-#     # 如果斜率为正且统计显著，则认为电机在上升
-#     return slope > 0.1  # 调整阈值以适应您的系统
-
-# if Touch_S>Touch_valve and Weight>0 and is_motor_rising(list(pos100_buffer)): # Update weight
-#     avg_force = np.mean(list(Srope100_buffer))
-#     Weight=avg_force
+                buffer_dyn_Stouch.append(Touch_S)
+                buffer_weight_Stouch.append(Touch_S)
 
 def main():
     try:
@@ -70,10 +71,12 @@ def main():
         print('Start Enable')
         myXYZ.OpenEnableZero_ALL()
         InitPos=myXYZ.Safe_Jog()
+        threading.Thread(target=read_rope_sensor, args=(), daemon=True).start()
+        threading.Thread(target=touch_sensor_server, args=(), daemon=True).start()
+        threading.Thread(target=read_cur_pos, args=(myXYZ,InitPos,), daemon=True).start()
         while Touch_S==0:
             time.sleep(0.1)
             print('Waiting Touch Data!!')
-        # time.sleep(10)
         
         Vgoal=0
         Vgoal_N=0
@@ -83,7 +86,7 @@ def main():
 
         while True:
             time.sleep(0.001)
-            if Ave_Touch_S>Touch_valve and Weight==0:
+            if np.mean(buffer_dyn_Stouch)>Touch_valve and Weight==0:
                 print('Enter Measurement!!!')
                 mode=1 # Measure Mode
                 cur_time = time.time()
@@ -91,30 +94,26 @@ def main():
                     # Vgoal=2000+0.005*Touch_S
                     Vgoal=2000
                     myXYZ.AxisMode_Jog(3,30,Vgoal)
-                    print('Waiting Lifting, Rope_S:',Rope_S, 'Touch_S:',Touch_S, 'Ave_Touch_S:',Ave_Touch_S, 'Elapsed time:',time.time()-cur_time)
+                    print('Waiting Lifting, Rope_S:',Rope_S, 'Elapsed time:',time.time()-cur_time)
                     time.sleep(0.01)
+                time.sleep(0.1)
                 if Rope_S>500:
-                    samples_ropeS=[]
-                    samples_TouchS=[]
-                    for i in range(1,50):
-                        samples_ropeS.append(Rope_S)
-                        samples_TouchS.append(Touch_S)
-                        time.sleep(0.01)
-                    Weight=np.mean(samples_ropeS)+0.2*np.mean(samples_TouchS)
+                    Weight=np.mean(buffer_weight_Srope)+0.2*np.mean(buffer_weight_Stouch)
                     print('Measured Success! Weight:', Weight)
                 else:
                     Vgoal=0
                     myXYZ.AxisMode_Jog(3,30,Vgoal)
-                    time.sleep(1.5)
+                    time.sleep(1)
                     print('Measured Failure: Over Time!')
-            if Ave_Touch_S<Touch_valve:
-                Weight=0
+            elif np.mean(buffer_dyn_Stouch)<Touch_valve:
                 mode=3 # Lossen Mode
-                err=40-Ave_Rope_S
+                Weight=0
+                err=40-np.mean(buffer_dyn_Srope)
                 Vgoal_N=40*err
             else:
                 mode=2 # Load Mode
-                err=Weight-Ave_Rope_S
+                if rising_slope > 2: Weight=np.mean(buffer_weight_Srope)+0.2*np.mean(buffer_weight_Stouch) # Update weight only when rising
+                err=Weight-np.mean(buffer_dyn_Srope)
                 if abs(err)<150:
                     err=0
                 Vgoal_N=15*err
@@ -128,8 +127,14 @@ def main():
 
             Pnum += 1
             if Pnum % 29 == 0:
-                print('Mode:',mode, 'Ave_Rope_S:',int(Ave_Rope_S), 'Ave_Touch_S:',int(Ave_Touch_S), 'Vgoal_N',int(Vgoal_N), 'Vgoal',int(Vgoal), 'diff:',int(diff), 'Weight', int(Weight))
-
+                print('Mode:',mode,
+                      'Ave3_Srope:',int(np.mean(buffer_dyn_Srope)),
+                      'Ave3_Stouch:',int(np.mean(buffer_dyn_Stouch)),
+                      'Vgoal_N',int(Vgoal_N),
+                      'Vgoal',int(Vgoal),
+                      'diff:',int(diff),
+                      'Weight', int(Weight),
+                      'slope: ', rising_slope)
             # myXYZ.AxisMode_Jog(3,30,-2000)
 
     except KeyboardInterrupt:
